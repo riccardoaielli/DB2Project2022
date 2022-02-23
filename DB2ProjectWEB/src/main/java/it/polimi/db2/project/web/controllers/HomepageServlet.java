084183921f154b3cace75e701883702acc0b2291
riccardo <riccardo@localhost>
2022-02-23 18:48:49 +0100

    Added some libraries, LogoutServlet and HomepageServlet and homepage.html skeleton

---
+++ DB2ProjectWEB/src/main/java/it/polimi/db2/project/web/controllers/HomepageServlet.java
@@ -0,0 +1,47 @@
+package it.polimi.db2.project.web.controllers;
+
+import it.polimi.db2.project.ejb.entities.*;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "HomepageServlet", value = "/homepage")
+public class HomepageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private TemplateEngine templateEngine;
+
+
+    public void init() {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession();
+        UserEntity user = (UserEntity) session.getAttribute("user");
+
+
+        resp.setContentType("text/html");
+
+        ServletContext servletContext = getServletContext();
+        WebContext ctx = new WebContext(req, resp, servletContext, req.getLocale());
+        String path = "/WEB-INF/homepage.html";
+
+        templateEngine.process(path, ctx, resp.getWriter());
+    }
+}
