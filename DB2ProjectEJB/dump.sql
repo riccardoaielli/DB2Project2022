de2def8b55de63596f6f820f8b4ca2982150e7cd
db2 <db2@db2-virtualbox>
2022-02-24 20:38:34 +0100

    DB update. The table to be created are the ones relative to internet service and phone service. There is also the need to specify the actions on insert, update and delete.

--- DB2ProjectEJB/dump.sql
+++ DB2ProjectEJB/dump.sql
@@ -1,33 +1,62 @@
-CREATE DATABASE  IF NOT EXISTS `db2Project`;
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: db2Project
+-- ------------------------------------------------------
+-- Server version	8.0.27-0ubuntu0.21.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `db2Project`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `db2Project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 USE `db2Project`;
 
 --
--- Table structure for table `admin`
+-- Table structure for table `alert`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `Username` varchar(45) NOT NULL,
-  `Password` varchar(45) NOT NULL,
+DROP TABLE IF EXISTS `alert`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alert` (
+  `Id` int NOT NULL,
+  `amount` int NOT NULL,
+  `timestamp` datetime(6) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE KEY `Username_UNIQUE` (`Username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_alert_1_idx` (`Id`),
+  CONSTRAINT `user_alert` FOREIGN KEY (`Id`) REFERENCES `user` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `admin`
+-- Dumping data for table `alert`
 --
 
-LOCK TABLES `admin` WRITE;
-INSERT INTO `admin` VALUES (1,'admin','admin');
+LOCK TABLES `alert` WRITE;
+/*!40000 ALTER TABLE `alert` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alert` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user`
+-- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Username` varchar(45) NOT NULL,
   `Password` varchar(45) NOT NULL,
@@ -36,11 +65,231 @@ CREATE TABLE `user` (
   UNIQUE KEY `Username_UNIQUE` (`Username`),
   UNIQUE KEY `Email_UNIQUE` (`Email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `optional_product`
+--
+
+DROP TABLE IF EXISTS `optional_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `optional_product` (
+  `Id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `fee` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `optional_product`
+--
+
+LOCK TABLES `optional_product` WRITE;
+/*!40000 ALTER TABLE `optional_product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `optional_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `cost` varchar(45) NOT NULL,
+  `start_date` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `timestamp` datetime(6) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service`
+--
+
+DROP TABLE IF EXISTS `service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service` (
+  `Id` int NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service`
+--
+
+LOCK TABLES `service` WRITE;
+/*!40000 ALTER TABLE `service` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service_oproduct`
+--
+
+DROP TABLE IF EXISTS `service_oproduct`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service_oproduct` (
+  `service_id` int NOT NULL,
+  `op_id` int NOT NULL,
+  PRIMARY KEY (`service_id`,`op_id`),
+  UNIQUE KEY `service_id_UNIQUE` (`service_id`),
+  UNIQUE KEY `op_id_UNIQUE` (`op_id`),
+  CONSTRAINT `oproduct` FOREIGN KEY (`op_id`) REFERENCES `optional_product` (`Id`),
+  CONSTRAINT `service` FOREIGN KEY (`service_id`) REFERENCES `service_pack` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service_oproduct`
+--
+
+LOCK TABLES `service_oproduct` WRITE;
+/*!40000 ALTER TABLE `service_oproduct` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service_oproduct` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service_pack`
+--
+
+DROP TABLE IF EXISTS `service_pack`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service_pack` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `validity_period` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  UNIQUE KEY `validity_period_UNIQUE` (`validity_period`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service_pack`
+--
+
+LOCK TABLES `service_pack` WRITE;
+/*!40000 ALTER TABLE `service_pack` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service_pack` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service_spack`
+--
+
+DROP TABLE IF EXISTS `service_spack`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service_spack` (
+  `service_id` int NOT NULL,
+  `spack_id` int NOT NULL,
+  KEY `service_id_idx` (`service_id`),
+  KEY `spack_id_idx` (`spack_id`),
+  CONSTRAINT `service_id` FOREIGN KEY (`service_id`) REFERENCES `service` (`Id`),
+  CONSTRAINT `spack_id` FOREIGN KEY (`spack_id`) REFERENCES `service_pack` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service_spack`
+--
+
+LOCK TABLES `service_spack` WRITE;
+/*!40000 ALTER TABLE `service_spack` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service_spack` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(45) NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  `Email` varchar(90) NOT NULL,
+  `Flag_Ins` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Username_UNIQUE` (`Username`),
+  UNIQUE KEY `Email_UNIQUE` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
 LOCK TABLES `user` WRITE;
-INSERT INTO `user` VALUES (1,'riccardo','riccardo','riccardo@example.org');
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `validity_period`
+--
+
+DROP TABLE IF EXISTS `validity_period`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `validity_period` (
+  `Id` int NOT NULL,
+  `monthly_fee` int NOT NULL,
+  `months` int NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `validity_period`
+--
+
+LOCK TABLES `validity_period` WRITE;
+/*!40000 ALTER TABLE `validity_period` DISABLE KEYS */;
+/*!40000 ALTER TABLE `validity_period` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-24 20:26:53
